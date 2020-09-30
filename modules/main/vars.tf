23bfb42cc30747f05f22adab494e1a4d70612635
koyande <64630031+koyande@users.noreply.github.com>
2020-09-30 12:48:54 +0530

    Update vars.tf

--- modules/main/vars.tf
+++ modules/main/vars.tf
@@ -1,13 +1,25 @@
 ### User ###
-variable "okta_user_first_name"{}
-variable "okta_user_last_name"{}
-variable "okta_user_login"{}
-variable "okta_user_email"{}
+variable "okta_user_first_name"{
+  default = "abc"
+  }
+variable "okta_user_last_name"{
+  default = "abc"
+  }
+variable "okta_user_login"{
+  default = "abc@example.com"
+  }
+variable "okta_user_email"{
+  default = "abc@example.com"
+  }
 
 ### Group ###
 
-variable "okta_group_name"{}
-variable "okta_group_description"{}
+variable "okta_group_name"{
+  default = "abc"
+  }
+variable "okta_group_description"{
+  default = "abc"
+  }
 
 ### Auth Server ###
 
@@ -19,7 +31,9 @@ variable "okta_auth_server_description" {
   default = "My Example Auth Server"
 }
  
-variable "okta_auth_server_name"{}
+variable "okta_auth_server_name"{
+  default = "abc"
+  }
  
 variable "okta_auth_server_issuer_mode" {
   default = "ORG_URL"
@@ -28,7 +42,9 @@ variable "okta_auth_server_issuer_mode" {
 
 ### App Saml ###
 
-variable "okta_app_saml_label"{}
+variable "okta_app_saml_label"{
+  default = "abc"
+  }
 
 variable "okta_app_saml_sso_url" {
   default = "http://example.com"
@@ -68,7 +84,9 @@ variable "okta_app_saml_honor_force_authn" {
 
 ### Inline Hook ###
 
-variable "okta_inline_hook_name"{}
+variable "okta_inline_hook_name"{
+  default = "abc"
+  }
 
 variable "okta_inline_hook_version" {
   default = "1.0.1"
@@ -89,7 +107,9 @@ variable "okta_idp_social_protocol_type" {
   default = "OAUTH2"
 }
 
-variable "okta_idp_social_name"{}
+variable "okta_idp_social_name"{
+  default = "abc"
+  }
 
 
 
